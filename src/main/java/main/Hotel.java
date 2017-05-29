6c074a63b1687da25823168dd5e91feb14100020
boschma2702 <reneboschma@hotmail.com>
2017-05-29 15:53:24 +0200

    added rooms

--- src/main/java/main/Hotel.java
+++ src/main/java/main/Hotel.java
@@ -1,5 +1,16 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotel {
 
+    List<Room> rooms;
+
+    public Hotel() {
+        rooms = new ArrayList<Room>();
+        for (int i = 0; i < 50; i++) {
+            rooms.add(new Room(i));
+        }
+    }
 }
---
+++ src/main/java/main/Room.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Room {
+
+    private int roomnumber;
+
+    public Room(int roomnumber) {
+        this.roomnumber = roomnumber;
+    }
+
+}
