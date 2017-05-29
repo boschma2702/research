922949edfff266142686252a7e10d2a9c772ab3f
boschma2702 <reneboschma@hotmail.com>
2017-05-29 16:00:58 +0200

    added employees

--- src/main/java/main/Hotel.java
+++ src/main/java/main/Hotel.java
@@ -6,11 +6,17 @@ import java.util.List;
 public class Hotel {
 
     List<Room> rooms;
+    List<Employee> employees;
 
     public Hotel() {
         rooms = new ArrayList<Room>();
         for (int i = 0; i < 50; i++) {
             rooms.add(new Room(i));
+        }
+
+        employees = new ArrayList<Employee>();
+        for(int i=0; i<10; i++){
+            employees.add(new Employee("first"+i, "last"+i, i));
         }
     }
 }
