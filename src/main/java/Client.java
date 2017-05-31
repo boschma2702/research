dc1fe5899f69879a3ea3698099afe6360bd8711d
boschma2702 <reneboschma@hotmail.com>
2017-05-31 11:21:33 +0200

    implemented client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,11 @@
 public class Client {
 
-    
+    private String firstname;
+    private String lastname;
+
+    public Client(String firstname, String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 
 }
