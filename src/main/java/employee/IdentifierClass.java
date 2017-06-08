cccb44571d26c6910caec3e35dfa84feb01e0f0a
boschma2702 <reneboschma@hotmail.com>
2017-06-08 10:06:40 +0200

    removed id class

--- src/main/java/employee/IdentifierClass.java
+++
@@ -1,9 +0,0 @@
-package employee;
-
-public class IdentifierClass {
-
-    int a;
-    int b;
-    int c;
-
-}
--- src/main/java/main/Client.java
+++ src/main/java/main/Client.java
@@ -1,3 +1,5 @@
+package main;
+
 public class Client {
 
     private String firstname;
